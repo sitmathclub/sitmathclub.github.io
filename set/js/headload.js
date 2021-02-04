bf524c27812c3a3471e5b74735c46e19621bda80
kotetsu0000 <bv19063@shibaura-it.ac.jp>
2021-02-05 00:01:22 +0900

    [update] いろいろ
    色々と細かい点の修正だったり、ドロップダウンメニューをCSSのみの実装に変更

--- set/js/headload.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    $("#headload").load("/set/html/head.html");
-});
---
+++ set/js/htmlload.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#subload").load("/set/html/sub.html");
+    $("#footload").load("/set/html/foot.html");
+    $("#headload").load("/set/html/head.html");
+});
